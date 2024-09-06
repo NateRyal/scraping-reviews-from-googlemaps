e114bf53358a56ff241e61e477ab297600db7424
NateRyal <nathan.ryalls@gmail.com>
2024-09-06 10:00:54 -0400

    Update env.py

--- env.py
+++ env.py
@@ -1,2 +1,2 @@
 URL = "https://www.google.com/maps/place/Colonial+Williamsburg/@37.2696927,-76.6961231,15z/data=!4m8!3m7!1s0x89b0890a32c9f1bf:0x73a6bdb2fce13e01!8m2!3d37.2696927!4d-76.6961231!9m1!1b1!16s%2Fm%2F06_1qm4?hl=en-US&entry=ttu&g_ep=EgoyMDI0MDgyOC4wIKXMDSoASAFQAw%3D%3D"
-DriverLocation = "./Driver/chromedriver.exe"
+DriverLocation = "C:\Users\nryalls\Downloads/chromedriver.exe"
